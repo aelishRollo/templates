0c2dab74f419b55429554b143e48868a8057f0b9
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2024-06-04 14:35:20 -0400

    localhost: 3000 now works

--- crud-express-mongodb/server.js
+++ crud-express-mongodb/server.js
@@ -1,1 +1,7 @@
 console.log('May the Node be with you')
+const express = require('express')
+const app = express()
+
+app.listen(3000, function () {
+    console.log('Listening on 3000, starchief')
+})
