1781dfe124c6e8eb80f4f8270bf6d3bf9cbaff63
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2024-06-04 14:44:44 -0400

    Add another page, foo.html

--- crud-express-mongodb/server.js
+++ crud-express-mongodb/server.js
@@ -10,3 +10,7 @@ app.listen(3000, function () {
 app.get('/', (req,res) => {     //determines what will go on the web page
     res.sendFile(__dirname + '/index.html')
 })
+
+app.get('/foo', (req,res) => {
+    res.sendFile(__dirname + '/foo.html')
+})
