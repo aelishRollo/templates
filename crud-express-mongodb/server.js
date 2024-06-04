3f22e0002ee2a9e6c9e352c8672a29b2f68a77c7
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2024-06-04 14:38:51 -0400

    Create app.get('/') endpoint

--- crud-express-mongodb/server.js
+++ crud-express-mongodb/server.js
@@ -5,3 +5,8 @@ const app = express()
 app.listen(3000, function () {
     console.log('Listening on 3000, starchief')
 })
+
+
+app.get('/', (req,res) => {     //determines what will go on the web page
+    res.send('Hello, world')
+})
