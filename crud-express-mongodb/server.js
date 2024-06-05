4e83d28b696dcf5dfaba76a0b4db282c277ea969
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2024-06-05 13:36:52 -0400

    Integrate bodyParser to process the information from the form

--- crud-express-mongodb/server.js
+++ crud-express-mongodb/server.js
@@ -1,6 +1,9 @@
-console.log('May the Node be with you')
+
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(3000, function () {
     console.log('Listening on 3000, starchief')
@@ -11,6 +14,10 @@ app.get('/', (req,res) => {     //determines what will go on the web page
     res.sendFile(__dirname + '/index.html')
 })
 
-app.get('/foo', (req,res) => {
+app.get('/foo', (req,res) => {      //serves foo.html at the endpoint /foo
     res.sendFile(__dirname + '/foo.html')
+})
+
+app.post('/quotes', (req,res) => {
+    console.log(req.body) // logs the value from the form in the input, as an object. It's req.body
 })
